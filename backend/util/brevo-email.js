8c943fbdce87f0459b7e22f4eac066bf3fed1931
Leobert Camoro <leobert.camoro101@gmail.com>
2026-08-05 13:05:07 +0800

    nodemailer to brevo fix forgot-password issue

---
+++ backend/util/brevo-email.js
@@ -0,0 +1,23 @@
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+defaultClient.authentications['api-key'].apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+const sendResetPasswordEmail = async (toEmail, resetToken) => {
+  const resetLink = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+
+  await apiInstance.sendTransacEmail({
+    sender: { email: process.env.GMAIL_USER, name: 'PlacePulse' },
+    to: [{ email: toEmail }],
+    subject: 'Reset your PlacePulse password',
+    htmlContent: `
+      <p>You requested a password reset for your PlacePulse account.</p>
+      <p><a href="${resetLink}">Click here to reset your password</a></p>
+      <p>This link will expire in 1 hour. If you didn't request this, you can safely ignore this email.</p>
+    `,
+  });
+};
+
+module.exports = sendResetPasswordEmail;
